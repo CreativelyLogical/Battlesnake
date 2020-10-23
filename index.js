c9384347cc9eaba704a5fc6e7d3984e85aa2b376
Umutjan Mahmut <umutjan2010@hotmail.com>
2020-10-22 17:48:04 -0700

    add basic collision avoidance logic

--- index.js
+++ index.js
@@ -32,25 +32,82 @@ function handleStart(request, response) {
   response.status(200).send('ok')
 }
 
+/*
+  head: Next coordinate of the snake's head based on move
+  move: The move that will be examined to see if it will lead to a collision
+  prevMove: The previous move of the snake
+  board: Data about the game board (contains info about the board's height, width, etc...)
+
+  Returns TRUE if the move will lead to a collision. Returns FALSE otherwise
+*/
+function willCollide(head, move , prevMove, board) {
+  // get the height & width of the game board
+  let boardHeight = board.height;
+  let boardWidth = board.width;
+
+  // opposite moves are illegal.
+  let oppositeMoves = new Map();
+
+  oppositeMoves['up'] = 'down';
+  oppositeMoves['down'] = 'up';
+  oppositeMoves['left'] = 'right';
+  oppositeMoves['right'] = 'left';
+
+  // eliminate the possibility of an opposite move
+  if (oppositeMoves[prevMove] === move) {
+    // If the current move is an opposite move of the previous move,
+    // then a collision with the self will occur
+    return true;
+  }
+
+  // eliminate collisions with the board
+  if (head.x < 0 || head.x > boardWidth - 1 || head.y < 0 || head.y > boardHeight - 1) {
+    // if the new head coordinates go out of bounds with either the x OR y axis, a collision will occur
+    return true;
+  }
+
+  return false;
+
+}
+
 function handleMove(request, response) {
   let gameData = request.body;
 
   // console.log(gameData);
+  let prevMove = gameData.you.shout;
+  let boardData = gameData.board;
 
+  // Declare what the next coordinate of the head will be based on the move
+  let nextHeadCoordinates = new Map();
+  nextHeadCoordinates['up'] = {
+    x: head.x,
+    y: head.y + 1,
+  };
+  nextHeadCoordinates['down'] = {
+    x: head.x,
+    y: head.y - 1,
+  };
+  nextHeadCoordinates['left'] = {
+    x: head.x - 1,
+    y: head.y,
+  };
+  nextHeadCoordinates['right'] = {
+    x: head.x + 1,
+    y: head.y,
+  };
 
 
   let possibleMoves = ['up', 'down', 'left', 'right'];
   // var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
 
-  let move = 'up';
+  let legalMoves = possibleMoves.filter(move => !willCollide(nextHeadCoordinates(move), move, prevMove, boardData));
+  
+  let move = legalMoves[Math.floor(Math.random() * legalMoves.length)];
 
-  if (gameData.you.head.y == 10) {
-    move = 'left';
-  }
-
-  console.log('MOVE: ' + move)
+  console.log('MOVE: ' + move);
   response.status(200).send({
-    move: move
+    move: move,
+    shout: move
   });
 }
 
